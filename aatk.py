cbbd3b8aaad9822bbd754f8078ff0109760f118c
Ambroise <ambroise.dufour@gmail.com>
2014-07-08 15:30:19 +0200

    created master file

---
+++ aatk.py
@@ -0,0 +1,14 @@
+
+"""
+An interface between netCDF4, pygrib and matplotlib's basemap
+"""
+
+import netcdf4.Dataset
+
+class Dataset :
+	def __init__(file_path, mode = 'r')
+		extension = split(file_path, '.')[1]
+		if extension == nc :
+			self = Dataset(file_path, mode)
+
+
