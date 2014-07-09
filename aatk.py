28ac9b3d020d9e3b63295760debcdcc59e6126b0
Ambroise <ambroise.dufour@gmail.com>
2014-07-09 14:57:54 +0200

    wrapped variable

--- aatk.py
+++ aatk.py
@@ -4,22 +4,36 @@ An interface between scipy, pandas, pygrib and matplotlib's basemap
 """
 
 #import numpy as np
-#import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io.netcdf import netcdf_file
 
 class Dataset :
 	def __init__(self, filePath, mode = 'r') :
 		if filePath.endswith('nc') :
-			self.kind = 'nc'
+			self.dataFormat = 'nc'
 			self.raw = netcdf_file(filePath, mode)
 
 			self.variableNames = self.raw.variables.keys()
 		else :
-			print "Not supported"
+			print "Format not supported"
 
 	def __getattr__(self, attributeName) :
 		if attributeName in self.variableNames :
-			return self.raw.variables[attributeName]
+			return Variable(
+				self,
+				self.raw.variables[attributeName].data)
 		else :
 			raise AttributeError
+
+
+class Variable :
+	def __init__(self, data) :
+		if dataset.dataFormat == 'nc' :
+			self.data = data
+		else :
+			print "Format not supported"
+	
+	def __getitem__(self, *args, **kwargs) :
+		return self.data.__getitem__(*args, **kwargs)
+
+
