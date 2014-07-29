c0b059fb09be41dcb04ac29b868cd6d99048e192
Ambroise Dufour <ambroise.dufour@gmail.com>
2014-07-29 09:52:26 +0200

    merged aa with variable

--- aa.py
+++ aa.py
@@ -3,11 +3,16 @@
 An interface between scipy, pygrib and matplotlib's basemap
 """
 
-from variable import Variable
-from variable import glazier
 from axis import Axis
 from axis import TimeAxis
 from axis import month
+
+import numpy as np
+import operator as op
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from mpl_toolkits.basemap import Basemap
+
 
 class DataMedium(object) :
 	def __getattr__(self, attributeName) :
@@ -18,7 +23,7 @@ class DataMedium(object) :
 		if attributeName == 't' :
 			return self.time
 		if attributeName == 'lat' :
-			return self.latitude :
+			return self.latitude
 		if attributeName == 'lon' :
 			return self.longitude
 		if attributeName == 'lev' :
@@ -27,9 +32,14 @@ class DataMedium(object) :
 			return self.level
 		if attributeName == 'level' :
 			return self.levels
+		if attributeName == 'lats' :
+			return self.latitude[:]
+		if attributeName == 'lons' :
+			return self.longitude[:]
+		if attributeName == 'levs' :
+			return self.level[:]
 		else :
 			raise AttributeError
-
 
 
 class File(DataMedium) :
@@ -48,6 +58,53 @@ class File(DataMedium) :
 		self._raw.close()
 		del self
 
+
+class Variable(DataMedium) :
+	def __init__(self, data=None, metadata={}, axes=OrderedDict()) :
+		self.axes = axes
+		self.metadata = metadata
+		if data != None :
+			self._data = data
+
+	def _get_data(self) :
+		return self._data
+	def _set_data(self, newValue) :
+		self._data = newValue
+	data = property(_get_data, _set_data)
+		
+	def __call__(self, **kwargs) :
+		multipleSlice = []
+		outputAxes = OrderedDict()
+		for axisName, axis in self.axes.iteritems() :
+			# must this axis be sliced ?
+			if axisName in kwargs.keys() :
+				# should a range of indices be extracted ?
+				if type(kwargs[axisName]) == tuple :
+					window = np.vectorize(glazier(kwargs[axisName]))	
+					# now extract the sub-axis corresponding to the conditions
+					mask = window(self.axes[axisName][:])
+					item = slice(
+							np.argmax(mask),
+							len(mask) - np.argmax(mask[::-1]))
+					outputAxes[axisName] = Axis(
+							self.axes[axisName][mask],
+							self.axes[axisName].units)
+				# extract a single index only
+				else :
+					item = np.argmax(
+						self.axes[axisName][:] == kwargs[axisName])
+					if item == 0 and \
+							self.axes[axisName][0] != kwargs[axisName] :
+						print "No match in " + axisName
+						return None
+					# don't add this axis to outputAxes
+			# leave the axis untouched
+			else :
+				item = slice(None)
+				outputAxes[axisName] = self.axes[axisName]
+			multipleSlice.append(item)
+		return Variable(self.data[multipleSlice], self.metadata, outputAxes)
+	
 
 def open(filePath) :
 	"Picks the appropriate File subclass to model a gridded data file"
