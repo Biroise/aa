1904979a17bdd7f91464c26ff7f0d57c4f7dd396
Ambroise Dufour <ambroise.dufour@gmail.com>
2014-07-29 10:47:30 +0200

    Manual repair of aa.py (due to premature push)

--- aa.py
+++ aa.py
@@ -57,7 +57,6 @@ class File(DataMedium) :
 		self._raw.close()
 		del self
 
-
 class Variable(DataMedium) :
 	def __init__(self, data=None, metadata={}, axes=OrderedDict()) :
 		self.axes = axes
@@ -104,6 +103,64 @@ class Variable(DataMedium) :
 			multipleSlice.append(item)
 		return Variable(self.data[multipleSlice], self.metadata, outputAxes)
 	
+	def mean(self, axes) :
+		return NotImplementedError
+
+	def _get_basemap(self) :
+		# assign to self a standard basemap
+		self._basemap = Basemap(
+				projection = 'cyl',
+				llcrnrlon = self.longitude.data.min(),
+				llcrnrlat = self.latitude.data.min(),
+				urcrnrlon = self.longitude.data.max(),
+				urcrnrlat = self.latitude.data.max())
+		return self._basemap
+	def _set_basemap(self, someMap) :
+		# user may set basemap himself
+		self._basemap = someMap
+	basemap = property(_get_basemap, _set_basemap)
+
+	@property
+	def plot(self) :
+		if len(self.axes) == 1 :
+			if self.axes.keys()[0] == 'levels' :
+				return plt.plot(self.data, self.axes['levels'])
+			else :
+				return plt.plot(self.axes.values()[0], self.data)
+		elif len(self.axes) == 2 :
+			if 'latitude' in self.axes.keys() and \
+					'longitude' in self.axes.keys() :
+				self.basemap.drawcoastlines()
+				x, y = self.basemap(
+					*np.meshgrid(self.longitude.data, self.latitude.data))
+				return self.basemap.pcolormesh(x, y, self.data)
+		else :
+			print "Variable has too many axes or none"
+
+
+def glazier(conditions) :
+	"The glazier makes windows : functions that test if conditions are met"
+	# if the user does not provide the type of boundaries
+	if len(conditions) == 2 :
+		# default boundaries are "closed-closed" unlike numpy
+		conditions = conditions + ('cc',)
+	# if the lower boundary is closed...
+	if conditions[2][0] == 'c' :
+		lowerCondition = op.ge
+	else :
+		lowerCondition = op.gt
+	# if the upper boundary is closed...
+	if conditions[2][1] == 'c' :
+		upperCondition = op.le
+	else :
+		upperCondition = op.lt
+	# extract the sub-axis related to the newConditions
+	def window(x) :
+		return lowerCondition(
+				x, conditions[0]) and \
+			upperCondition(
+				x, conditions[1])
+	return window
 
 def open(filePath) :
 	"Picks the appropriate File subclass to model a gridded data file"
