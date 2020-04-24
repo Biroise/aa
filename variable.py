bug solved : newMetadata loaded by averager

variable.py
@@ -97,7 +97,7 @@ class Variable(object) :
         # ideally : slice the pressure field as well except! level slice
         # for now let's play it safe
         newMetadata = {key:value for key, value in self.metadata.iteritems()
-                if key not in ['surfacePressure', 'thickness', 'maskedFraction']}
+                if key not in ['oceanDepth', 'surfacePressure', 'thickness', 'maskedFraction']}
         # slice the maskedFraction exactly the same way
         if 'maskedFraction' in self.metadata :
             newMetadata['maskedFraction'] = self.metadata['maskedFraction'](**kwargs)
@@ -107,6 +107,8 @@ class Variable(object) :
                 newMetadata['thickness'] = self.metadata['thickness'](**kwargs)
             if 'surfacePressure' in self.metadata :
                 newMetadata['surfacePressure'] = self.metadata['surfacePressure'](**kwargs)
+            if 'oceanDepth' in self.metadata :
+                newMetadata['oceanDepth'] = self.metadata['oceanDepth'](**kwargs)
         # dispatch the conditions to the axes
         for axisName, condition in kwargs.iteritems() :
             item, newAxis = self.axes[axisName](condition)
@@ -196,7 +198,7 @@ class Variable(object) :
         if len(axisNames) > 0 :
             # copy the metadata (by reference) - no point in copying surface pressure
             newMetadata = {key:value for key, value in self.metadata.iteritems()
-                    if key not in ['surfacePressure', 'thickness', 'maskedFraction']}
+                    if key not in ['oceanDepth', 'surfacePressure', 'thickness', 'maskedFraction']}
             # extract the name of the axis to be averaged
             axisName = axisNames.pop(0)
             newAxes = self.axes.copy()
@@ -209,6 +211,8 @@ class Variable(object) :
             del newAxes[axisName]
             if axisName == 'level' and 'surfacePressure' in self.metadata :
                 self.metadata['thickness'] = statistics.sp2thck(self)
+            if axisName == 'level' and 'oceanDepth' in self.metadata :
+                self.metadata['thickness'] = statistics.od2thck(self)
             if axisName == 'level' and 'thickness' in self.metadata :
                 return Variable(
                             data = np.nansum(self.data*self.thickness.data,
@@ -247,7 +251,7 @@ class Variable(object) :
                             data = np.nanmean(self.data*weights[weightSlice]/np.nanmean(weights),\
                                     axis=axisIndex),
                             axes = newAxes,
-                            metadata = self.metadata.copy()
+                            metadata = newMetadata
                         ).averager(axisNames)
         # no axes left to average : return the result
         else :