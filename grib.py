e02ca5688cafc6ac0c71b0afa2db9646b4adcb8e
Ambroise Dufour <ambroise.dufour@posteo.net>
2020-07-03 15:52:15 +0300

    corrected deprecated slice in grib

--- grib.py
+++ grib.py
@@ -457,9 +457,9 @@ class Variable(aa.Variable) :
                     gribLines = gribIndex(**subConditions)
                     for member in newConditions['member'] :
                         if twistedLongitudes :
-                            self._data[lineIndex, ..., slice1] = \
+                            self._data[tuple([lineIndex, Ellipsis, slice1])] = \
                                 gribLines[member].values[mask]
-                            self._data[lineIndex, ..., slice2] = \
+                            self._data[tuple([lineIndex, Ellipsis, slice2])] = \
                                 gribLines[member].values[secondMask]
                         else :
                             self._data[lineIndex] = gribLines[member].values[mask]
